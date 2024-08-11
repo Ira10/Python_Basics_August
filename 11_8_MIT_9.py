40a7875bd076348252b4c2649e19032d0f7f8f1d
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-11 22:09:48 +0530

    Update 11_8_MIT_9.py

--- 11_8_MIT_9.py
+++ 11_8_MIT_9.py
@@ -1,57 +1,57 @@
-# print(max(1,5))
+print(max(1,5))
 
-# print('\n')
+print('\n')
 
-# print(max(1,5,4,2,4,21,3,442,2111))
+print(max(1,5,4,2,4,21,3,442,2111))
 
-# ###### checking out on my own
-# def sum(*args):
+###### checking out on my own
+def sum(*args):
 
-#     sum_ = 0
-#     for a in args:
-#         sum_ += a 
-#     return sum_
+    sum_ = 0
+    for a in args:
+        sum_ += a 
+    return sum_
 
-# print(sum())
-
-
-# #### chatgpt, how to take user input seperated by spaces
-
-# def sum(*args):
-#     sum_ = 0
-#     for a in args:
-#         sum_ += a 
-#     return sum_
-
-# # Taking input from the user
-# user_input = input("Enter numbers separated by spaces: ")
-
-# # Splitting the input string into individual numbers, converting them to integers or floats, and passing them to the sum function
-# numbers = map(int, user_input.split())
-
-# # Unpacking the list of numbers into the sum function
-# print(sum(*numbers))
+print(sum())
 
 
-#### chatgpt, how to take user input seperated by comma
+#### chatgpt, how to take user input seperated by spaces
 
-# def sum(*args):
-#     sum_ = 0
-#     for a in args:
-#         sum_ += a 
-#     return sum_
+def sum(*args):
+    sum_ = 0
+    for a in args:
+        sum_ += a 
+    return sum_
 
-# # Taking input from the user
-# user_input = input("Enter numbers separated by comma: ")
+# Taking input from the user
+user_input = input("Enter numbers separated by spaces: ")
 
-# # Splitting the input string into individual numbers, converting them to integers or floats, and passing them to the sum function
-# numbers = map(int, user_input.split(','))
+# Splitting the input string into individual numbers, converting them to integers or floats, and passing them to the sum function
+numbers = map(int, user_input.split())
 
-# # Unpacking the list of numbers into the sum function
-# print(sum(*numbers))
-# print('\n')
-# print(numbers)
-# print('\n')
+# Unpacking the list of numbers into the sum function
+print(sum(*numbers))
+
+
+### chatgpt, how to take user input seperated by comma
+
+def sum(*args):
+    sum_ = 0
+    for a in args:
+        sum_ += a 
+    return sum_
+
+# Taking input from the user
+user_input = input("Enter numbers separated by comma: ")
+
+# Splitting the input string into individual numbers, converting them to integers or floats, and passing them to the sum function
+numbers = map(int, user_input.split(','))
+
+# Unpacking the list of numbers into the sum function
+print(sum(*numbers))
+print('\n')
+print(numbers)
+print('\n')
 
 
 
